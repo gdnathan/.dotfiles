aa9b25d52f4ef8ede9872f78e542c3cb445f168c
nathan gd <nathan.gardet-derc@pm.me>
2022-03-23 12:10:06 +0100

    adding bass plugin to fish and nvm

--- fish.sh
+++ fish.sh
@@ -1,1 +1,8 @@
+if ! which yay > /dev/null; then
+    sudo -Syu fish fisher nvm
+else
+    echo "please manually install the package 'fish', 'fisher' and 'nvm'"
+fi
+
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
