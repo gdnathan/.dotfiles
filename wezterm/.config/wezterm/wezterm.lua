13c5cd68fae9872a9585298ea9ed039975632297
nathan gd <nathan.gardet-derc@pm.me>
2022-03-24 18:22:38 +0100

    changing terminal from alacritty to wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+return {
+    color_scheme = "Dracula",
+
+    --window_background_image = "/path/to/wallpaper.jpg",
+
+    --window_background_image_hsb = {
+        --brightness = 0.3,
+    --}
+}
